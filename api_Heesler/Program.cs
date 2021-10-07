9959cd06bf4624205c6e280e2e0aa53aad0c71b1
Heesler21499 <54820695+Heesler21499@users.noreply.github.com>
2021-10-06 23:53:21 -0600

    Esta crud Sync

---
+++ api_Heesler/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+//Heesler Estuardo López Barraza 0910-12-567
+
+namespace web_api_dbempresa
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
